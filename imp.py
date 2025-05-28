36ec7f629b3259e176f6b81d970adb5fc1b49c1d
raj07102002 <rajchandrawanshi69@gmail.com>
2025-05-28 22:18:39 +0530

    intial

---
+++ imp.py
@@ -0,0 +1,46 @@
+# Q.1
+# s=set()
+# s.add(20)
+# s.add(20.)
+# s.add("20")
+# print(len(s)) 
+
+# Q.2 
+# d={}
+# name=input("Enter the name :")
+# lang=input("Enter the lang :")
+# d.update({name:lang}) 
+
+# name=input("Enter the name :")
+# lang=input("Enter the lang :")
+# d.update({name:lang}) 
+
+# name=input("Enter the name :")
+# lang=input("Enter the lang :")
+# d.update({name:lang}) 
+
+
+# # name=input("Enter the name :")
+# # lang=input("Enter the lang :")
+# # d.update({name:lang}) 
+
+# # name=input("Enter the name :")
+# # lang=input("Enter the lang :")
+# # d.update({name:lang}) 
+
+# print(d)  
+
+a=int(input("Enter the value:")) 
+
+if(a>=18): 
+    print("yes i am 18 above") 
+
+elif(a<0): 
+    print("IT a negtive number")
+    print("There is no meaning you enter this value")
+    print("Try again") 
+elif(a==0):
+    print("Try again") 
+else: 
+    print("invalid") 
+print("End the program")
