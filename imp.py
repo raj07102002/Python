bb4220de710c4213a9e41a7343e5fda5a973fa7f
raj07102002 <rajchandrawanshi69@gmail.com>
2025-05-30 15:53:31 +0530

    newone

--- imp.py
+++ imp.py
@@ -29,18 +29,23 @@
 # # d.update({name:lang}) 
 
 # print(d)  
+# Q.3
+# a=int(input("Enter the value:")) 
 
-a=int(input("Enter the value:")) 
+# if(a>=18): 
+#     print("yes i am 18 above") 
 
-if(a>=18): 
-    print("yes i am 18 above") 
+# elif(a<0): 
+#     print("IT a negtive number")
+#     print("There is no meaning you enter this value")
+#     print("Try again") 
+# elif(a==0):
+#     print("Try again") 
+# else: 
+#     print("invalid") 
+# print("End the program") 
 
-elif(a<0): 
-    print("IT a negtive number")
-    print("There is no meaning you enter this value")
-    print("Try again") 
-elif(a==0):
-    print("Try again") 
-else: 
-    print("invalid") 
-print("End the program")
+i=0
+for i in range(1,6) : 
+    
+    print(i)
