39e9e4ddac66130148fb9f6e2325b3beee4d51b1
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-06 08:55:11 +0530

    p

--- imp.py
+++ imp.py
@@ -102,4 +102,26 @@
 # i=i-2
 # while i>=0: 
 #     print(" "*i+" *"*(n-i))
-#     i=i-1
+#     i=i-1 
+
+# Q.7 armstrong 
+
+# n=int(input("enter any value = "))
+# x=y=n
+# digit=0
+# sum=0
+# while n>0: 
+#     digit=digit+1
+#     n=n//10
+# print(digit)
+# print(n)
+# while x>0: 
+#     last_digit=x%10
+#     sum=sum+last_digit**digit
+#     x=x//10
+# print(sum)
+# print(x)
+# if y==sum: 
+#     print(f'given no. {y} is armstrong no.')
+# else: 
+#     print(f'given no. {y} is not a armstrong no.')
