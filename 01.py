b46681f8873b2b37e6bfb61aad219137f29c559e
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-08 22:57:16 +0530

    b

--- 01.py
+++ 01.py
@@ -2,9 +2,20 @@
 # b=2
 # print(a+b) 
 
-# Q.1 
+# # Q.1 
 
-extra_classes=["vocal classes","english","maths","hindi","python","java"]
-print("In the 2021 you will have do this co-curriculam attivites")
-for subject in extra_classes:
-    print(subject)
+# extra_classes=["vocal classes","english","maths","hindi","python","java"]
+# print("In the 2021 you will have do this co-curriculam attivites")
+# for subject in extra_classes:
+#     print(subject) 
+
+# Q.2 
+
+result=True
+for marks in 65,45,39,16,20:
+    if marks<35:
+        result=False
+        break
+        print(marks) 
+    if (not(result)):
+        print("fail")
