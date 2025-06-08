ca38038943c7bfd6901708ee4030e7d0ad098287
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-08 22:44:09 +0530

    e

--- 01.py
+++ 01.py
@@ -1,3 +1,10 @@
-a=1
-b=2
-print(a+b)
+# a=1
+# b=2
+# print(a+b) 
+
+# Q.1 
+
+extra_classes=["vocal classes","english","maths","hindi","python","java"]
+print("In the 2021 you will have do this co-curriculam attivites")
+for subject in extra_classes:
+    print(subject)
