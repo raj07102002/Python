be53007e73e8666e1cfaa51b8c466a2cf163387d
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-09 19:54:31 +0530

    len

--- practices.py
+++ practices.py
@@ -1,4 +1,12 @@
+# i=0
+# while i<5 : 
+#     print("Raj",end=",")
+#     i+=1 
+
+# Q.1 
+
+l=['1','Raj','lakshman','janki','jai bolo hanuman ki']
 i=0
-while i<5 : 
-    print("Raj",end=",")
+while i<(len(l)): 
+    print(l[i])
     i+=1
