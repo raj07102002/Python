8397e9007e77fb94714f7edd783ec3fcda0d7e2f
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-09 19:34:23 +0530

    F

--- 01.py
+++ 01.py
@@ -11,11 +11,23 @@
 
 # Q.2 
 
-result=True
-for marks in 65,45,39,16,20:
-    if marks<35:
-        result=False
-        break
-        print(marks) 
-    if (not(result)):
-        print("fail")
+# result=True
+# for marks in 65,45,39,16,20:
+#     if marks<35:
+#         result=False
+#         break
+#         print(marks) 
+#     if (not(result)):
+#         print("fail") 
+
+Q.3 
+def new(**n):
+    print(n)
+    print(type(n))
+    for k,v in n.items():
+        print(f'my key  is {k} and value is={v}')
+    for k in n.keys():
+        print(f'my key is={k}')
+    for v in n.values():
+        print(f'my value is={v}')
+new(name="Raj",age=22,qulifi="b.tech")
