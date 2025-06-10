6df3e465eec599f7177e7ce17316fe1785c4870f
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-10 21:33:41 +0530

    one

--- Numpy.py
+++ Numpy.py
@@ -1,16 +1,24 @@
-import numpy as np 
-import time 
-size=10000000
-l1=range(size)
-l2=range(size)
-start=time.time()
-result=[x+y for x,y in zip(l1,l2)] 
-end =time.time() 
-print("python list is: ",end -start) 
+# import numpy as np 
+# import time 
+# size=10000000
+# l1=range(size)
+# l2=range(size)
+# start=time.time()
+# result=[x+y for x,y in zip(l1,l2)] 
+# end =time.time() 
+# print("python list is: ",end -start) 
 
-a1=np.arange(size)
-a2=np.arange(size)
-start=time.time()
-result=a1+a2
-end=time.time()
-print("python array :",end - start)
+# a1=np.arange(size)
+# a2=np.arange(size)
+# start=time.time()
+# result=a1+a2
+# end=time.time()
+# print("python array :",end - start) 
+
+# Q.2 
+# # conversion of list into the array
+# l=[]
+# for i in range(1,6):
+#     a=input("Enter the value")
+#     l.append(a) 
+# print(l)
