2bde116d5a1265f9b1bd880f3fa1d41db5091b46
raj07102002 <rajchandrawanshi69@gmail.com>
2025-07-04 22:31:04 +0530

    one

--- Questions.py
+++ Questions.py
@@ -1,10 +1,17 @@
-Q1. 
+# Q1. 
 
-if n % 2 == 1:
-    print("Weird")
-elif n % 2 == 0 and 2 <= n <= 5:
-    print("Not Weird")
-elif n % 2 == 0 and 6 <= n <= 20:
-    print("Weird")
-elif n % 2 == 0 and n > 20:
-    print("Not Weird")
+# if n % 2 == 1:
+#     print("Weird")
+# elif n % 2 == 0 and 2 <= n <= 5:
+#     print("Not Weird")
+# elif n % 2 == 0 and 6 <= n <= 20:
+#     print("Weird")
+# elif n % 2 == 0 and n > 20:
+#     print("Not Weird") 
+
+# Q.2 
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     for i in range(1, n + 1):
+#         print(i, end='')
