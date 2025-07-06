06ab7e8495005d7012e7c9d163c60bc3324a59bb
raj07102002 <rajchandrawanshi69@gmail.com>
2025-07-06 22:22:59 +0530

    z

--- Questions.py
+++ Questions.py
@@ -15,3 +15,16 @@
 #     n = int(input())
 #     for i in range(1, n + 1):
 #         print(i, end='')
+
+# Q.3 
+
+# n = int(input())
+# if n < 2:
+#     print("Not Prime")
+# else:
+#     for i in range(2, int(n**0.5) + 1):
+#         if n % i == 0:
+#             print("Not Prime")
+#             break
+#     else:
+#         print("Prime")
