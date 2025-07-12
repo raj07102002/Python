6093df1d5960aa4db850fe1888e7dd96e83adb3e
raj07102002 <rajchandrawanshi69@gmail.com>
2025-07-12 23:50:08 +0530

    two

--- Questions.py
+++ Questions.py
@@ -78,3 +78,13 @@
 #     total += digit
 #     n = n // 10
 # print(total) 
+
+# Q.7
+
+# n = 123
+# total = 0
+# while n > 0:
+#     digit = n % 10
+#     total += digit
+#     n = n // 10
+# print(total)
