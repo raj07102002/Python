767012acd4c952a0a8bdf6c37bfcffc1e72c1c20
raj07102002 <rajchandrawanshi69@gmail.com>
2025-07-19 23:06:47 +0530

    push

---
+++ Question1.py
@@ -0,0 +1,24 @@
+# Q.1
+# import pyttsx3
+# engine = pyttsx3.init()
+
+# # For Mac, If you face error related to "pyobjc" when running the `init()` method :
+# # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
+
+# engine.say("hello raj chandrawashi me i help you")
+# engine.runAndWait() 
+
+# Q.2 
+
+# import os
+
+# # Set the directory path (you can also use "." for current directory)
+# directory_path = "."
+
+# # Get the list of files and folders
+# contents = os.listdir(directory_path)
+
+# # Print the contents
+# print(f"Contents of '{directory_path}':")
+# for item in contents:
+#     print(item)
